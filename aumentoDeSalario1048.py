a06fc516a531d28dfe39dc8ac69b80c6066ae9c5
javanlacerda <javan.lacerda@ccc.ufcg.edu.br>
2017-06-19 09:56:41 -0300

    add uri1048

---
+++ aumentoDeSalario1048.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+money = float(raw_input())
+
+if 0 <= money <= 400:
+	percent, reajuste = 15, (0.15 * money)
+	
+elif 400 < money <= 800:
+	percent, reajuste = 12, (0.12 * money)
+	
+elif 800 < money <= 1200:
+	percent, reajuste = 10, (0.10 * money)
+	
+elif 1200 < money <= 2000:
+	percent, reajuste = 7, (0.07 * money)	
+	
+else:
+	percent, reajuste = 4, (0.04 * money)
+	
+print "Novo salario: %.2f" % (money + reajuste)
+print "Reajuste ganho: %.2f" % (reajuste)
+print "Em percentual: %i %%"	% (percent)
