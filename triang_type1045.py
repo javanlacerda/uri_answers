64a800a754d797e475cfd7ffdc091188b115092d
javanlacerda <javan.lacerda@ccc.ufcg.edu.br>
2017-06-19 00:46:12 -0300

    uri 1045

---
+++ triang_type1045.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+entry = raw_input().split()
+
+a = float(entry[0])
+b = float(entry[1])
+c = float(entry[2])
+
+if a >= b + c or b >= c + a or c >= a + b:
+	print "NAO FORMA TRIANGULO"
+	
+else:
+	if a ** 2 == b ** 2 + c ** 2 or b ** 2 == c ** 2 + a ** 2 or c ** 2 == a ** 2 + b ** 2:
+		print "TRIANGULO RETANGULO"
+	
+	elif a ** 2 > b ** 2 + c ** 2 or b ** 2 > c ** 2 + a ** 2 or c ** 2 > a ** 2 + b ** 2:	
+		print "TRIANGULO OBTUSANGULO"
+	
+	elif a ** 2 < b ** 2 + c ** 2 or b ** 2 < c ** 2 + a ** 2 or c ** 2 < a ** 2 + b ** 2:	
+		print "TRIANGULO ACUTANGULO"
+		
+	
+	if a == b and b == c:
+		print "TRIANGULO EQUILATERO"
+	
+	elif a == b and b != c or b == c and c != a or c == a and a != b:
+		print "TRIANGULO ISOSCELES"		
